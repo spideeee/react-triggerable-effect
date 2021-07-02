bd1e1a96e053a3e2771b25486f490575e6de617d
spideeee <salimkhan1708@gmail.com>
2021-07-02 10:39:05 +0530

    Memoized trigger callback

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 export default function useTrigerrableEffect(callback, dependencies){
     const [triggerValue, triggerEffect] = useState(false);
     useEffect(callback, [triggerValue, ...dependencies]);
-    return () => triggerEffect(value => !value);
+    return useCallback(() => triggerEffect(value => !value), []);
 }
